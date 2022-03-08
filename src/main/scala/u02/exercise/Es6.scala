7c6aeadac72659435cbb956ab93ae833e5c91ca6
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-08 16:18:17 +0100

    es6 tail

--- src/main/scala/u02/exercise/Es6.scala
+++ src/main/scala/u02/exercise/Es6.scala
@@ -5,3 +5,12 @@ object Es6:
     case 0 => 0
     case 1 => 1
     case n => fib(n - 2) + fib(n - 1)
+
+  def fibTail(n: Int): Int =
+    @annotation.tailrec
+    def _fib(n: Int, acc2: Int, acc1: Int): Int = n match
+      case 0 => acc2
+      case 1 => acc1
+      case n => _fib(n - 1, acc1, acc2 + acc1)
+
+    _fib(n, 0, 1)
--- src/test/scala/u02/Es6Test.scala
+++ src/test/scala/u02/Es6Test.scala
@@ -2,9 +2,12 @@ package u02
 
 import org.junit.Test
 import org.junit.Assert.assertEquals
-import u02.exercise.Es6.fib
+import u02.exercise.Es6.{fib, fibTail}
 
 class Es6Test {
   @Test def testFib(): Unit =
     assertEquals((0, 1, 1, 2, 3), (fib(0), fib(1), fib(2), fib(3), fib(4)))
+
+  @Test def testFibTail(): Unit =
+    assertEquals((0, 1, 1, 2, 3), (fibTail(0), fibTail(1), fibTail(2), fibTail(3), fibTail(4)))
 }
