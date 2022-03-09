8b5445570c4cf6b306ac71abbaf4bec18c35f825
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-09 19:37:15 +0100

    refactor es4

--- src/main/scala/u02/exercise/Es4.scala
+++ src/main/scala/u02/exercise/Es4.scala
@@ -2,6 +2,9 @@ package u02.exercise
 
 object Es4:
   val p1: Double => Double => Double => Boolean = x => y => z => x <= y && y <= z
+
   val p2: (Double, Double, Double) => Boolean = (x, y, z) => x <= y && y <= z
+
   def p3(x: Double)(y: Double)(z: Double) = x <= y && y <= z
+
   def p4(x: Double, y: Double, z: Double) = x <= y && y <= z
