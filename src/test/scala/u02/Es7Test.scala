b1e8696775e60050ed788f4229bfe9feb371ab97
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-09 20:09:09 +0100

    refactor test es7

--- src/test/scala/u02/Es7Test.scala
+++ src/test/scala/u02/Es7Test.scala
@@ -6,22 +6,24 @@ import u02.exercise.Es7.Shape
 import u02.exercise.Es7.Shape.{Rectangle, Square, Circle, area, perimeter}
 
 class Es7Test {
+  val delta = 10e-10
+
   @Test def testRectanglePerimeter(): Unit =
-    assertEquals(30, perimeter(Rectangle(10,5)), 10e-10)
+    assertEquals(30, perimeter(Rectangle(10,5)), delta)
 
   @Test def testRectangleArea(): Unit =
-    assertEquals(50, area(Rectangle(10,5)), 10e-10)
+    assertEquals(50, area(Rectangle(10,5)), delta)
 
   @Test def testSquarePerimeter(): Unit =
-    assertEquals(40, perimeter(Square(10)), 10e-10)
+    assertEquals(40, perimeter(Square(10)), delta)
 
   @Test def testSquareArea(): Unit =
-    assertEquals(100, area(Square(10)), 10e-10)
+    assertEquals(100, area(Square(10)), delta)
 
   @Test def testCirclePerimeter(): Unit =
-    assertEquals(25.12, perimeter(Circle(4)), 10e-10)
+    assertEquals(25.12, perimeter(Circle(4)), delta)
 
   @Test def testCircleArea(): Unit =
-    assertEquals(50.24, area(Circle(4)), 10e-10)
+    assertEquals(50.24, area(Circle(4)), delta)
 
 }
