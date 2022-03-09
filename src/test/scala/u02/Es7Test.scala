8913e4755d440912cbd59305f27516bc4f729c8b
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-09 14:19:28 +0100

    refactor test es7

--- src/test/scala/u02/Es7Test.scala
+++ src/test/scala/u02/Es7Test.scala
@@ -3,25 +3,25 @@ package u02
 import org.junit.Test
 import org.junit.Assert.assertEquals
 import u02.exercise.Es7.Shape
-import u02.exercise.Es7.Shape.{area, perimeter}
+import u02.exercise.Es7.Shape.{Rectangle, Square, Circle, area, perimeter}
 
 class Es7Test {
   @Test def testRectanglePerimeter(): Unit =
-    assertEquals(30, perimeter(Shape.Rectangle(10,5)), 10e-10)
+    assertEquals(30, perimeter(Rectangle(10,5)), 10e-10)
 
   @Test def testRectangleArea(): Unit =
-    assertEquals(50, area(Shape.Rectangle(10,5)), 10e-10)
+    assertEquals(50, area(Rectangle(10,5)), 10e-10)
 
   @Test def testSquarePerimeter(): Unit =
-    assertEquals(40, perimeter(Shape.Square(10)), 10e-10)
+    assertEquals(40, perimeter(Square(10)), 10e-10)
 
   @Test def testSquareArea(): Unit =
-    assertEquals(100, area(Shape.Square(10)), 10e-10)
+    assertEquals(100, area(Square(10)), 10e-10)
 
   @Test def testCirclePerimeter(): Unit =
-    assertEquals(25.12, perimeter(Shape.Circle(4)), 10e-10)
+    assertEquals(25.12, perimeter(Circle(4)), 10e-10)
 
   @Test def testCircleArea(): Unit =
-    assertEquals(50.24, area(Shape.Circle(4)), 10e-10)
+    assertEquals(50.24, area(Circle(4)), 10e-10)
 
 }
