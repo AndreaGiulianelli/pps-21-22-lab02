cd7318f713feb71814a46b7ff149d465628c35b2
andrea.giulianelli <andrea.giulianelli4@studio.unibo.it>
2022-03-11 13:52:50 +0100

    types corrections

--- src/main/scala/u02/exercise/Es8.scala
+++ src/main/scala/u02/exercise/Es8.scala
@@ -18,15 +18,15 @@ object Es8:
       case Some(a) => f(a)
       case _ => None()
 
-    def filter[A, B >: A](opt: Option[A])(f: B => Boolean): Option[A] = opt match
+    def filter[A](opt: Option[A])(f: A => Boolean): Option[A] = opt match
       case Some(value) if f(value) => opt
       case _ => None()
 
-    def map[A, B >: A, C](opt: Option[A])(f: B => C): Option[C] = opt match
+    def map[A, B](opt: Option[A])(f: A => B): Option[B] = opt match
       case Some(value) => Some(f(value))
       case _ => None()
 
     // f return an Option in order to permit to return None() to a composition
-    def map2[A, B, C >: A, D >: B, E](opt1: Option[A])(opt2: Option[B])(f: (C, D) => Option[E]): Option[E] = (opt1, opt2) match
+    def map2[A, B, C](opt1: Option[A])(opt2: Option[B])(f: (A, B) => Option[C]): Option[C] = (opt1, opt2) match
       case (Some(value1), Some(value2)) => f(value1, value2)
       case _ => None()
